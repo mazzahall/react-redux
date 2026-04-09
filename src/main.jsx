3819120026a68cf4c6557a18717c79b4492ccec9
mazzahall <abdullah0210@student.abudzar.sch.id>
2026-04-09 11:13:35 +0700

    gasss

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Counter />} />
           <Route path={"/counter"} element={
             <>
            <Counter />
